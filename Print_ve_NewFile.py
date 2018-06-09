f6dca1103521606285c06d4d42a784959eda8e1c
YasarGuden <35410494+YasarGuden@users.noreply.github.com>
2018-06-10 01:09:07 +0300

    Add files via upload

---
+++ Print_ve_NewFile.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 10 00:50:35 2018
+
+@author: yasar
+"""
+
+"""Aritmetik_işlemler ve Stringler dosyalarımızı şuana kadar idle da yazdık
+ve sonucları aldık artık programlama yapagımız kısıma geçiyoruz.Yeni bir sayfaya...
+İdle da sol üst köşe duran File(dosya) kısmına girelim.Orada New File(yeni dosya)
+tıklıyoruz ve karşımıza gelecekte yüzlerce satır ile dolu olucak bir boş sayfa geldi.
+Bu sayfa da şimdi ye kadar öğrendiklerimizi yazarsak hiç bir şey cıktı olarak vermiyecektir.
+Çünkü derleyici ye ekrana bastırma komudu vermedik.Bunun için ilk gömülü fonksiyonumuzu
+inceliyelim.   Print """
+
+'Yazılım' # bu ifadeyi çıktı ekranında görmek için yapıcagmız şey şu
+
+print 'Yazılım' #Bu fonksiyon Python Derleyicisine ekrana bastır der.
+
+print 'Python ile'
+
+print "kolay anlaşılır"
+
+#Print'in bilmemiz gereken özelliği bu diğer özelliklerini ileride örnekler arasında kullanıcaz
+
+#İdle ile artık işimiz kalmadı.Bize ekran çıktısını new file in verdiğiniz artık biliyoruz
+"""Önerilen programlar 
+1.Spider (benim kullandığım)
+2.Atom text editör
+3.Sublime 3 text editör
+4.Pycharm 
+5.Linux distiribitörü kullanıyor iseniz: vi,gedit terminal ile kullanılır ise
+çok avantjlı bir hal alıyor.Şiddetle tavsiye ederim"""
