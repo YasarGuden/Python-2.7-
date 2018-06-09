5c3f5579f79dd774ce36d6db11d5b99de30d57f5
YasarGuden <35410494+YasarGuden@users.noreply.github.com>
2018-06-10 00:24:20 +0300

    Add files via upload

---
+++ Aritmetik_işlemler.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  9 22:45:31 2018
+
+@author: yasar
+"""
+
+#Pythonda tam sayıları otomatik olarak algılar sadece sayı yazmak yeterlidir.
+#Pythonda aritmatik işlem yapamak kullandığımız 4 işlem ile aynıdır.
+
+"""Toplama işlemi"""
+3+4
+"""Çıkarma işlemi"""
+10-3
+"""Çarpma işlemi"""
+3*3  
+"""Bölme işlemi"""
+10/2
+"""Burada bir parantez açmak istiyorum bazı videolarda ve kitaplarda "//" operatör
+ile karşılaşabilirsiniz.Bu operatör bölme işleminden kalanı ondalık sayı olarak
+ vermez yani 10//3 işlemi sonuç olarak 3 sonucu verir."""
+ 
+ """Üst işlemi"""
+ 3^2  #Üst alma işaretini yaparken(Shift+3) gözükmez bir tuşa daha basınca cıkar
+ 
+ """Mod alma işlemi"""
+ 15%2
